5387521f76c98fefbabc2f46cd482cb1d4e16ab4
Meghsham-Atgeir <145336570+Meghsham-Atgeir@users.noreply.github.com>
2023-11-01 21:39:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,9 +16,9 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
+# Let's put a pick list here so they can pick the fruit they want to include
 default_fruits = ['Avocado', 'Strawberries']
 selected_fruits = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), default_fruits)
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_to_show = my_fruit_list.loc[selected_fruits]  # Use the correct variable name
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
